0c707c1d01a3d964c73b8f336ed326e7d799c78f
Hash <takuya21hashimoto@gmail.com>
2012-12-29 16:30:46 +0900

    ログイン後ユーザ保存まで

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,11 +3,11 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def zaim
     @user = User.find_or_create_user_by_zaim_oauth(request.env["omniauth.auth"], current_user) # 最初current_userはnil
     if @user.persisted?
-      sign_in_and_redirect root_path #NOTE:commentout   @user, :event => :authentication #this will throw if @user is not activated
+      sign_in_and_redirect @user #NOTE:commentout  , :event => :authentication #this will throw if @user is not activated
       set_flash_message(:notice, :success, :kind => "Zaim") if is_navigational_format?
     else
       #session["devise.facebook_data"] = request.env["omniauth.auth"]
-      redirect_to new_user_registration_url
+      redirect_to root_path #new_user_registration_url
     end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,9 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :omniauthable
+  ### devise :database_authenticatable, :registerable,
+  ###        :recoverable, :rememberable, :trackable, :omniauthable
+  devise :omniauthable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :name, :provider, :uid
