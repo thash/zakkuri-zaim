a5bee22bbf0656b24b2affea662b6a9e758a6ad7
Hash <takuya21hashimoto@gmail.com>
2012-12-29 14:25:01 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ZakkuriZaim::Application.config.secret_token = 'a6995fc26922b5ab61a1ef24ddf9a2e5add1c9ce6d2b1ce0f81fedd9e05d7841fcaaa299ac2e9552ef47e3bca4478d27ad557e3d1281a3cb0c31796c488cf6e2'
