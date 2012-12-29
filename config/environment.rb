0d1d0f61ef487b8f65f00986a48cb7edabf0984c
Hash <takuya21hashimoto@gmail.com>
2012-12-29 18:00:23 +0900

    setting.yml

--- config/environment.rb
+++ config/environment.rb
@@ -1,9 +1,8 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-# OmniAuth認証連携したので不要
-# # bundlerのhashieを読み込んだ後. 開発用token
-# $secret = Hashie::Mash.new(YAML.load_file(File.expand_path('../secret.yml', __FILE__)))
+# 置き換える
+$setting = Hashie::Mash.new(YAML.load_file(File.expand_path('../setting.yml', __FILE__)))
 
 
 # Initialize the rails application
