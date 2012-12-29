6743c858acf083fdc027d0ea8b3b2763057a1e65
Hash <takuya21hashimoto@gmail.com>
2012-12-29 23:38:30 +0900

    deviseのfilterで認証をチェック

--- app/controllers/zaim_api_controller.rb
+++ app/controllers/zaim_api_controller.rb
@@ -1,7 +1,9 @@
 # coding: utf-8
 class ZaimApiController < ApplicationController
+  before_filter :authenticate_user!
   def pay
-    zaimapi.pay!( params["category_id"], params["genre_id"], params["price"] )
+    z = ZaimApi.new(current_user.token, current_user.token_secret)
+    z.pay!( params["category_id"], params["genre_id"], params["price"] )
     flash[:notice] = "記録しました"
   rescue ZaimApi::UnauthorizedError
     flash[:alert] = "認証エラーが発生"
@@ -9,11 +11,5 @@ class ZaimApiController < ApplicationController
     flash[:alert] = "APIエラーが発生"
   ensure
     redirect_to root_path
-  end
-
-  def zaimapi
-    return nil unless signed_in?
-    # リクエスト回数を減らすためインスタンス変数化してnil guard
-    @zaimapi ||= ZaimApi.new(current_user.token, current_user.token_secret)
   end
 end
