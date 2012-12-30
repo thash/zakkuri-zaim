1585438aa2482f13ef92828b21fc2fbdd62a9e10
Hash <takuya21hashimoto@gmail.com>
2012-12-30 17:19:04 +0900

    ログアウト機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 ZakkuriZaim::Application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
+  devise_scope :user do
+    get "/logout", :to => "devise/sessions#destroy"
+  end
+
   post 'zaim/pay' => 'zaim_api#pay'
 
   get "home/index"
